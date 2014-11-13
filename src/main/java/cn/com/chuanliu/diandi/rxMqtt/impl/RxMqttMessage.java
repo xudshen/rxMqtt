421f939b33cd5e76e6762c32bc387419e07d58dd
xudshen <xudshen@hotmail.com>
2014-11-13 21:27:32 +0800

    add a decrypt method

--- src/main/java/cn/com/chuanliu/diandi/rxMqtt/impl/RxMqttMessage.java
+++ src/main/java/cn/com/chuanliu/diandi/rxMqtt/impl/RxMqttMessage.java
@@ -2,6 +2,12 @@ package cn.com.chuanliu.diandi.rxMqtt.impl;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 import cn.com.chuanliu.diandi.rxMqtt.interfaces.IRxMqttMessage;
 
 /**
@@ -10,6 +16,7 @@ import cn.com.chuanliu.diandi.rxMqtt.interfaces.IRxMqttMessage;
 public class RxMqttMessage implements IRxMqttMessage {
     private String topic;
     private MqttMessage rxMessage;
+    private String decrypted_payload = null;
 
     public RxMqttMessage(String topic, MqttMessage rxMessage) {
         this.topic = topic;
@@ -23,7 +30,18 @@ public class RxMqttMessage implements IRxMqttMessage {
 
     @Override
     public String getMessage() {
-        return null == rxMessage ? null : new String(rxMessage.getPayload());
+        return null == rxMessage ? null : (decrypted_payload != null ? decrypted_payload : new String(rxMessage.getPayload()));
+    }
+
+    public String decrypt(Cipher dcipher, SecretKey key) {
+        try {
+            byte[] payload = rxMessage.getPayload();
+            dcipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(Arrays.copyOfRange(payload, 0, 16)));
+            decrypted_payload = new String(dcipher.doFinal(Arrays.copyOfRange(payload, 16, payload.length)));
+            return decrypted_payload;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
